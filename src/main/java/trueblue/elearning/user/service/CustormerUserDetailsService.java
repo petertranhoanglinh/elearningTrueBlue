4a31fa0b7c23f1f8e88257226a2cdecf5847f416
petertranhoanglinh <73209234+petertranhoanglinh@users.noreply.github.com>
2021-08-02 18:02:34 +0700

    ád
    hi

--- src/main/java/trueblue/elearning/user/service/CustormerUserDetailsService.java
+++ src/main/java/trueblue/elearning/user/service/CustormerUserDetailsService.java
@@ -25,33 +25,45 @@ public class CustormerUserDetailsService  implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+		Boolean check = false;
+		String password1 = "";
+		String username1 = "";
 		List<UserModel> user = new ArrayList<UserModel>();
 		user =     _userdao.ListGroup();
 		
+		
 		for(int i = 0; i <= user.size(); i++) {
-			if(user.get(i).getEmail().equals(username)) {
-				List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
-				GrantedAuthority autoAuthority = new SimpleGrantedAuthority("ADMIND");
-				grantList.add(autoAuthority);
-				
-				
-			   String password =  user.get(i).getPassword();
-		       UserDetails userDetails = new User(username, password, grantList);
-		       return userDetails;
-		    		   
-			}else {
-			     new UsernameNotFoundException("loginFail");
-			}
-			return null;
 			
+			if(user.get(i).getEmail().equals(username)) {
+				username1 = user.get(i).getEmail();
+				password1 = user.get(i).getPassword();
+				check = true;
+				break;
+			}
+			
+			
+		 }
+		if(check == true){
+			System.out.println(username1);
+			System.out.println(password1);
+			List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+			GrantedAuthority autoAuthority = new SimpleGrantedAuthority("ADMIND");
+			grantList.add(autoAuthority);
+	        UserDetails userDetails = new User(username1, password1, grantList);
+	       return userDetails;
+		}else{
+			new UsernameNotFoundException("loginFail");
 		}
+	
+		
+		
+		return null;
 		
 		
 		
 		
 	    
-		return null;
+	
 	}
 
 }
